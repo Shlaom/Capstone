cba935a35239577b80b5a1ffa83b8ea2576b79e0
eneleh758 <78579586+eneleh758@users.noreply.github.com>
2022-06-22 18:49:41 +0900

    filter choice

--- masking/urls.py
+++ masking/urls.py
@@ -7,10 +7,24 @@ app_name = 'masking'
 urlpatterns = [
     path('', views.main, name='main'),
 
+    path('registration', views.registration, name='registration'),
+
+    path('face_capture', views.face_capture, name='face_capture'),
+
     path('video', views.video, name='video'),
 
     path('home', views.home, name='home'),
 
     # 마이페이지
     path('mypage', views.mypage, name='mypage'),
+
+    path('masking_on', views.masking_on, name='masking_on'),
+
+    path('masking_off', views.masking_off, name='masking_off'),
+
+    path('mode_mosaic', views.mode_mosaic, name='mode_mosaic'),
+
+    path('mode_blur', views.mode_blur, name='mode_blur'),
+
+    path('mode_imaging', views.mode_imaging, name='mode_imaging'),
 ]
